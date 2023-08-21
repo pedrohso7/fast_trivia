508130fd73e0db2b1fe228d1bc5a8e7f618e60db
Pedro Oliveira <pholiveira1998@hotmail.com>
2023-08-21 10:23:38 -0400

    refactor(appbar): refactor default app bar to handle back actions.

--- lib/core/widgets/default_appbar.dart
+++ lib/core/widgets/default_appbar.dart
@@ -4,23 +4,32 @@ import '../constants/image_path.dart';
 import '../constants/sizes.dart';
 
 class DefaultAppbar extends StatelessWidget implements PreferredSizeWidget {
+  final bool withBackButton;
+  final VoidCallback? onPressBackButton;
   const DefaultAppbar({
     Key? key,
+    this.withBackButton = true,
+    this.onPressBackButton,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: AppColors.yellow,
-      leading: const Padding(
-        padding: EdgeInsets.all(AppSizes.s8),
-        child: CircleAvatar(
-          backgroundColor: AppColors.white,
-          child: Icon(
-            Icons.account_balance,
-            color: AppColors.yellow,
-          ),
-        ),
+      leading: Padding(
+        padding: const EdgeInsets.all(AppSizes.s8),
+        child: (withBackButton == true)
+            ? IconButton(
+                icon: const Icon(Icons.chevron_left),
+                onPressed: onPressBackButton,
+              )
+            : const CircleAvatar(
+                backgroundColor: AppColors.white,
+                child: Icon(
+                  Icons.account_balance,
+                  color: AppColors.yellow,
+                ),
+              ),
       ),
       centerTitle: true,
       title: Image.asset(
